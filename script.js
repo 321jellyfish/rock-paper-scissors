3dc9a875d70106d5fb2cad2130c7710bf6d83952
Charlotte <csljeffreys@gmail.com>
2022-02-27 15:21:09 +0000

    Add UI

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
 
 let playerScore = 0;
 let computerScore = 0;
+let playerSelection;
+let computerSelection = computerPlay();
+
+const rockButton = document.querySelector('#rock-button');
+const paperButton = document.querySelector('#paper-button');
+const scissorsButton = document.querySelector('#scissors-button');
+
+const newGame = document.querySelector('.newGame');
+
+newGame.disabled = true;
+
+const resultParas = document.querySelector('.resultParas');
+const userGamePlay = document.querySelector('.userGamePlay');
+const computerGamePlay = document.querySelector('.computerGamePlay');
+const gameResult = document.querySelector('.gameResult');
+const scoreBoard = document.querySelector('.scoreBoard');
+const finalResult = document.querySelector('.finalResult');
+
+
+rockButton.addEventListener('click', rockGame);
+paperButton.addEventListener('click', paperGame);
+scissorsButton.addEventListener('click', scissorsGame);
+
+newGame.addEventListener('click', reset);
+
+function reset(){
+    rockButton.disabled = false;
+    paperButton.disabled = false;
+    scissorsButton.disabled = false;
+    userGamePlay.textContent = "";
+    computerGamePlay.textContent = "";
+    gameResult.textContent = "";
+    scoreBoard.textContent = "";
+    finalResult.textContent = "";
+    playerScore = 0;
+    computerScore = 0;
+
+}
 
 /*Generate function that returns random results of Rock, Paper or Scissors*/
 function computerPlay(){
@@ -14,6 +52,7 @@ function computerPlay(){
         return "SCISSORS";
     }
 }
+
 
 /*Create function that plays one game of RPS, comparing two answers one for computer and one  for player, returns string with result*/
 function playRound(playerSelection, computerSelection){
@@ -46,32 +85,87 @@ playerSelection = playerSelection.toUpperCase();
 
 }
 
-/*Creat function that plays five games */
-function game(){
+/*Create function that plays one rock, paper, scissor game */
+function rockGame(){
 
-    
-    for (let gameCounter = 0; gameCounter < 5; gameCounter++) {
-
-        let playerSelection = prompt("Play five games of Rock, Paper, Scissors against the computer! Type in your choice!");
+        let playerSelection = "Rock"; 
         let computerSelection = computerPlay();
 
-        console.log(`You played ${playerSelection}`);
-        console.log(`Computer played ${computerSelection}`);
+        userGamePlay.textContent = `You played ${playerSelection.toLowerCase()}` 
+        computerGamePlay.textContent = `Computer played ${computerSelection.toLowerCase()}`;
 
-        console.log(playRound(playerSelection, computerSelection));
+       
 
-        console.log(`Player Score: ${playerScore} | Computer Score: ${computerScore}`);
-      }
+        gameResult.textContent = (playRound(playerSelection, computerSelection));
 
-    if (playerScore === computerScore){
-        console.log("Overall draw! :/");
-    } else if (playerScore > computerScore){
-        console.log("You win overall! :)");
-    } else {
-        console.log("You lose overall :(");
-    }
+        scoreBoard.textContent = `Player Score: ${playerScore} | Computer Score: ${computerScore}`;
+        
+        if((playerScore === 5)||(computerScore === 5)){
+            document.getElementById("rock-button").disabled = true; 
+            document.getElementById("paper-button").disabled = true; 
+            document.getElementById("scissors-button").disabled = true; 
+            newGame.disabled = false;
+            if(playerScore > computerScore){
+                finalResult.textContent = "END OF GAME - You win!";
+            } else {
+                finalResult.textContent = "END OF GAME - Computer wins!";
+            }
+        }
+
 }
 
-game();
+function paperGame(){
 
-/*Add something to deal with player answers an incorrect string or cancels"*/
+    let playerSelection = "Paper"; 
+    let computerSelection = computerPlay();
+
+    userGamePlay.textContent = `You played ${playerSelection.toLowerCase()}` 
+    computerGamePlay.textContent = `Computer played ${computerSelection.toLowerCase()}`;
+    
+
+
+    gameResult.textContent = (playRound(playerSelection, computerSelection));
+
+    scoreBoard.textContent = `Player Score: ${playerScore} | Computer Score: ${computerScore}`;
+
+    if((playerScore === 5)||(computerScore === 5)){
+        document.getElementById("rock-button").disabled = true;
+        document.getElementById("paper-button").disabled = true; 
+        document.getElementById("scissors-button").disabled = true; 
+        newGame.disabled = false;
+        if(playerScore > computerScore){
+            finalResult.textContent = "END OF GAME - You win!";
+        } else {
+            finalResult.textContent = "END OF GAME - Computer wins!";
+        }
+
+        
+    }
+
+}
+
+function scissorsGame(){
+
+    let playerSelection = "Scissors"; 
+    let computerSelection = computerPlay();
+
+    userGamePlay.textContent = `You played ${playerSelection.toLowerCase()}` 
+    computerGamePlay.textContent = `Computer played ${computerSelection.toLowerCase()}`;
+
+    gameResult.textContent = (playRound(playerSelection, computerSelection));
+
+    scoreBoard.textContent = `Player Score: ${playerScore} | Computer Score: ${computerScore}`;
+
+    if((playerScore === 5)||(computerScore === 5)){
+        document.getElementById("rock-button").disabled = true;
+        document.getElementById("paper-button").disabled = true; 
+        document.getElementById("scissors-button").disabled = true;
+        newGame.disabled = false; 
+        if(playerScore > computerScore){
+            finalResult.textContent = "END OF GAME - You win!";
+        } else {
+            finalResult.textContent = "END OF GAME - Computer wins!";
+        }
+    }
+
+}
